2e761f2f09fa8c1d4bb4a319fddcdfc44fdbb410
miladyyyy <phylosfia@gmail.com>
2023-02-02 19:55:56 +0800

    2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import store from '@/store'
+
 import './styles/base.css'
 import './styles/index.css'
 Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
+  render: h => h(App),
+  store,
 }).$mount('#app')
